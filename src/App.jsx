63bffa56b82c45c5fff9abbef410118b5ca39d4a
mateus <mateus.borja@ufpi.edu.br>
2025-03-03 22:22:28 -0300

    adicionando importação

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login/Login';
 import Registration from './Components/Registration/Registration'
+import Home from './Components/Home/Home';
 
 function App() {
   
